319307535ea736082c5e7cd756c51f994fdadd31
a-e3aguari <a-e3aguari@corp.generali.net>
2024-04-17 14:48:22 +0200

    removed sp credential from provider block

--- version.tf
+++ version.tf
@@ -4,17 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "> 3.59.0, <= 3.77.0"
+      version = "> 3.59.0, <= 3.90.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
-
-  subscription_id = "4275b7ef-d10b-4b46-a5ad-e5ff0a1dd9c5"
-  tenant_id         = "33ab2861-ffcf-45c4-870c-12dd75f4b254"
-  client_id         = "54c0e46b-41fa-40b0-b4fa-1f9078c174a4"
-  client_secret     = "PO-8Q~yNVgMD_k31zruKcHpxsBBfpb61b.aAudA0"
-
 }
