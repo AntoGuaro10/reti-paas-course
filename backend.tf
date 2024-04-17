654128826a46a439adcf78e4ca0d18d9b026ece5
a-e3aguari <a-e3aguari@corp.generali.net>
2024-04-17 12:15:26 +0200

    changed value on backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "azurerm" {
-      resource_group_name  = var.resource_group
-      storage_account_name = var.storageaccount-tfstate
+      resource_group_name  = "RGPAASCOURSE"
+      storage_account_name = "paasstgaccount1"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
   }
