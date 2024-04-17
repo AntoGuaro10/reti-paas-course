d6f868a51304e9ca4fb75a0c50db7fc90d699ce8
a-e3aguari <a-e3aguari@corp.generali.net>
2024-04-17 13:09:09 +0200

    set credential in azure provider block

--- version.tf
+++ version.tf
@@ -8,3 +8,13 @@ terraform {
     }
   }
 }
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "4275b7ef-d10b-4b46-a5ad-e5ff0a1dd9c5"
+  tenant_id         = "33ab2861-ffcf-45c4-870c-12dd75f4b254"
+  client_id         = "54c0e46b-41fa-40b0-b4fa-1f9078c174a4"
+  client_secret     = "PO-8Q~yNVgMD_k31zruKcHpxsBBfpb61b.aAudA0"
+
+}
